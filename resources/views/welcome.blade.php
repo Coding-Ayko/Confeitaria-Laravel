d7b659bdcf46ab128099f4d68c770f469aefdb16
Coding-ayko <tauane581730@gmail.com>
2024-01-24 14:40:41 -0300

    Inserção dos estilos do Header

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,10 +1,14 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
-        <title>@yield('title')Laravel</title>
+        <title>@yield('title')Aykandi</title>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="stylesheet" href="css\style.css">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Merriweather:ital,wght@1,900&family=Outfit:wght@300&family=Quicksand:wght@500&family=Sansita+Swashed:wght@300&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Inter:wght@600&family=Merriweather:ital,wght@1,900&family=Miniver&family=Outfit:wght@300&family=Quicksand:wght@500&display=swap" rel="stylesheet">
         <script src="js\script.js"></script>
         <script src="uikit/dist/js/uikit-icons.min.js"></script>
     </head>
@@ -12,66 +16,80 @@
     @yield('content')
     <header>
     <div class="div-header-img">
-      <a href="./index.php"><img src="/./img/Aykandi.svg" fill="black" alt="Aykandi"></a>
+      <a href="./index.php"><img style=" width: 218px; height: 32px; color:blue;" src="/./img/AykandiHeader.svg" alt="Aykandi"></a>
     </div>
     <div>
       <nav>
-        <ul class="">
-          <li><a href="./.php">Página Inicial</a></li>
-          <li><a href="./.php">Meus Favoritos</a></li>
-          <li><a href="./.php">Receitas</a></li>
-          <li><a href="./.php">Contato</a></li>
+        <ul class="menu">
+            <li class="menu"><a href="./.php">Página Inicial</a></li>
+            <li><a href="./.php">News</a></li>
+            <li><a href="./.php">Receitas</a></li>
+            <li><a href="./.php">Artigos</a></li>
+            <li><a href="./.php">Favoritos</a></li>
+            <li><a href="./.php">Contato</a></li>
         </ul>
       </nav>
     </div>
-    <div class="div-header-nav-login">
+
+    <div class="header-acesso">
       <nav>
-        <ul class="">
-          <li><a href="./.php">Login</a></li>
+        <ul class="login">
+          <li id="login">
+            <a  class="cta" href="./.php">
+                Login 
+                <svg width="13px" height="10px" viewBox="0 0 13 10">
+                    <path d="M1,5 L11,5"></path>
+                    <polyline points="8 1 12 5 8 9"></polyline>
+                </svg> 
+            </a>
+        </li>
           <li><a href="./.php">Cadastre-se</a></li>
         </ul>
       </nav>
     </div>
+    
+</header>
+<hr>
 
-    </header>
+    <!-- <div class="main-content">
+    <h2>Confeitaria Gourmet</h2>
+    <p>Doces Saudáveis e Deliciosos!</p>
+    <button>Faça seu pedido</button>
+    <button>Conheça nossos doces</button>
+    </div> -->
+
     <footer>
     <footer class="footer-bg">
-    <div class="footer container">
+    <div class="footer-container">
       <div>
-        <a href="./index.html"><img src="/./img/Aykandi.svg" alt="Bikcraft"></a>
+        <a href="./index.html"><img style=" width: 218px; height: 32px;"  src="/./img/Aykandi.svg" alt="Bikcraft"></a>
       </div>
-
-      <div class="contato">
-        <h2 class="font-2-l-b cor-0">CONTATO</h2>
-          <ul class="contato-item font-2-m">
-            <li><a href="tel:+551699999999">+55 16 9999-9999</li></a>
-            <li><a href="mailto:contato@bikcraft.com">contato@bikcraft.com</li></a>
-            <li>Rua Ali Perto, 27 - Botafogo</li>
-            <li> Rio de Janeiro - RJ</li>
-          </ul>
-          <ul class="redes font-2-m">
-            <li><img src="./img/redes/instagram.svg" alt="logo instagram"></li>
-            <li><img src="./img/redes/facebook.svg" alt="logo facebook"></li>
-            <li><img src="./img/redes/youtube.svg" alt="logo youtube"></li>
-          </ul>
-      </div>
-
-      <div class="informacoes">
-        <h2 class="font-2-l-b cor-0">INFORMAÇÕES</h2>
+      
+      <div class="">
         <nav>
-        <ul class="font-1-m cor-5">
-          <li><a href="./bicicletas.html">Home</a></li>
-          <li><a href="./seguros.html">Quem nós somos?</a></li>
-          <li><a href="./contato.html">Receitas</a></li>
-          <li><a href="./contato.html">Contato</a></li>
+        <ul class="">
+          <li><a href="./.html">HOME</a></li>
+          <li><a href="./.html">NEWS</a></li>
+          <li><a href="./.html">RECEITAS</a></li>
+          <li><a href="./.html">ARTIGOS</a></li>
+          <li><a href="./.html">SOBRE</a></li>
+          <li><a href="./.html">CONTATO</a></li>
         </ul>
         </nav>
       </div>
-
-      <p class="direitos font-2-m cor-6">Aykandi. &copy; Todos os direitos reservados. </p>
+      <div>
+      <form class="ai_inputWr__Pz3eQ">
+        <span class="">Endereço de e-mail</span>
+        <input type="text" name="promptInput" value="">
+        <button>Generate</button>
+    </form>
+      </div>
+      <div>
+          <p class="">&copy; 2024 | Todos os direitos reservados.</p>
+      </div>
     </div>
-  </footer>
     </footer>
-    
     </body>
 </html>
+
+
