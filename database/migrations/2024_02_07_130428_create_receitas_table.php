e81910fa9a700f3ecc14aea02227706bd4ec9fcb
TauaneCarolinaBarao <168219073+tauaneCarolinaBarao@users.noreply.github.com>
2025-04-16 15:42:45 -0300

    add fields to receitas table and rename category to id_category (requires doctrine/dbal)
    Feat: Created a migration to update the receitas table and installed the required composer extensions (doctrine/dbal)

--- database/migrations/2024_02_07_130428_create_receitas_table.php
+++ database/migrations/2024_02_07_130428_create_receitas_table.php
@@ -10,7 +10,8 @@ return new class extends Migration
      * Run the migrations.
      */
     public function up(): void
-    {
+    {   
+        //alteração de tabela para receber mais parâmetros. 
         Schema::create('receitas', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
@@ -19,6 +20,15 @@ return new class extends Migration
             $table->string("category");
             $table->integer("rating");
             $table->boolean("private"); 
+        // necessidade de adicionar os seguintes campos
+        //$table->text("ingredients);
+        //$table->text("prepare_mode");
+        //$table->string("time",10);
+        //$table->string("rendiment",100);
+        //table->boolean("chefs_favourite")->default(false);
+        //tem que alterar o nome da coluna category para id_category
+
+
 
         });
     }
---
+++ database/migrations/2025_04_16_122905_update_receitas_table_add_fields_and_rename_category.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('receitas', function (Blueprint $table) {
+            // Need to add the following fields:
+            $table->text("ingredients");
+            $table->text("prepare_mode");
+            $table->string("time", 10);
+            $table->string("rendiment", 100);
+            $table->boolean("chefs_favourite")->default(false);
+
+            // To rename the column 'category' to 'id_category', it was necessary to run:
+            // composer require doctrine/dbal
+            $table->renameColumn("category", "id_category");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('receitas', function (Blueprint $table) {
+            // Revert changes
+            $table->dropColumn("ingredients", "prepare_mode", "time", "rendiment", "chefs_favourite");
+            $table->renameColumn("id_category","category");
+        });
+    }
+};
