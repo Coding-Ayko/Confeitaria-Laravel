2b1ea32dc54228a4dfb0dcd89b43ad42254badf9
Maria Eduarda Martins <martins.maedu@gmail.com>
2025-08-18 11:18:48 -0300

    Adiciona anotações sobre a animação adicionada anteriormente

--- resources/views/cadastre.blade.php
+++ resources/views/cadastre.blade.php
@@ -25,14 +25,29 @@
     </section>
 
     <script src="{{ asset('js/gsap/gsap.min.js') }}"></script>
-    <script src="{{ asset('js/splittext.min.js') }}"></script>
-    <script src="{{ asset('js/animacao-cadastro.js') }}"></script>
 
     <script>
+        // ease: Define como a velocidade da animação varia ao longo do tempo.
+        // delay: Está ligado ao tempo de espera antes de a animação começar.
+        // ease, tipos:
+        // back.in, back.out, back.inOut (fazem o movimento passar um pouco do alvo e voltar, tipo elástico)
+        // elastic.in, elastic.out, elastic.inOut (aquele efeito de mola mesmo)
+        // bounce.in, bounce.out, bounce.inOut (efeito quicando)
+        // rough, steps(n), e até custom eases com curvas Bézier
+        // power1.in, power1.out, power1.inOut: são apenas funções de aceleração (easing) que definem como a animação acontece ao longo do tempo
+        // power1 → curva mais suave, aceleração/desaceleração leve.
+        // power2 → curva mais acentuada, acelera/desacelera mais rápido.
+        // power3 → ainda mais intensa.
+        // power4 → bem intensa, efeito mais dramático de aceleração.
+        // Overshoot é o quanto o elemento “passa do ponto final” antes de voltar para ele.
+        // O número dentro do parêntese define a intensidade do overshoot (o quanto o elemento “passa do alvo” antes de voltar).
         document.addEventListener("DOMContentLoaded", () => {
             gsap.from(".cadastre-form h1", {
+                // vai se mover 50px para esquerda
                 y: 50,
+                // controla a transparência, neste caso totalmente invisvel
                 opacity: 0,
+                // durará 2s
                 duration: 1,
                 ease: "back.out(1.7)"
             });
